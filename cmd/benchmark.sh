1098b2e9a0e03c8353df0a444c0e5d415dd4a5d7
RomanBoegli <roman-boegli@hotmail.com>
2022-04-11 22:34:27 +0200

    bash script to perform a bunch of benchm. at once

---
+++ cmd/benchmark.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+
+# This script assumes the docker containers are already up and running.
+
+####################
+#### VARIABLES #####
+####################
+
+script_set="employees"
+
+# general
+db_host="127.0.0.1"
+MULTIPLICITIES=("10" "20")
+threads=15
+gobench_main_path="/Users/rbo/Documents/Gits/gobench/cmd/main.go"
+script_base_path="/Users/rbo/Documents/Gits/gobench/scripts"
+result_base_path="/Users/rbo/Documents/Gits/gobench/tmp/results"
+
+# mysql
+mysql_port="3306"
+mysql_user="root"
+mysql_pass="password"
+
+# neo4j
+neo_port="7687"
+neo_user="neo4j"
+neo_pass="password"
+
+# postgres
+postgres_port="5432"
+postgres_user="postgres"
+postgres_pass="password"
+
+
+########################
+#### ACTUAL SCRIPT #####
+########################
+
+start_time=`date +%s`
+echo -e "\nSTART BENCHMARKING...\n"
+for MULT in "${MULTIPLICITIES[@]}"; do
+    echo $(for i in $(seq 1 50); do printf "_"; done) 
+    echo -e "\nITERATIONS: ${MULT}"
+    
+    echo -e "\nTEST MYSQL"
+    go run $gobench_main_path mysql \
+        --host $db_host \
+        --port $mysql_port \
+        --user $mysql_user \
+        --pass $mysql_pass \
+        --iter $MULT \
+        --script "${script_base_path}/${script_set}/mysql.sql" \
+        --writecsv "${result_base_path}/${script_set}/mysql_${iter}.csv"
+
+    echo -e "\nTEST NEO4J"
+    go run $gobench_main_path neo4j \
+        --host $db_host \
+        --port $neo_port \
+        --user $neo_user \
+        --pass $neo_pass \
+        --iter $MULT \
+        --script "${script_base_path}/${script_set}/neo4j.cql" \
+        --writecsv "${result_base_path}/${script_set}/neo4j_${iter}.csv"
+
+    echo -e "\nTEST POSTGRES"
+    go run $gobench_main_path postgres \
+        --host $db_host \
+        --port $postgres_port \
+        --user $postgres_user \
+        --pass $postgres_pass \
+        --iter $MULT \
+        --script "${script_base_path}/${script_set}/postgres.sql" \
+        --writecsv "${result_base_path}/${script_set}/postgres_${MULT}.csv"
+done
+
+echo -e "\n"
+echo $(for i in $(seq 1 50); do printf "#"; done) 
+echo -e "\nTOTAL RUN TIME: " $(expr `date +%s` - $start_time) s
