a1c46bf6d4058d7240a531da7459cd205a441882
RomanBoegli <roman-boegli@hotmail.com>
2022-04-11 23:31:58 +0200

    extend script to also merge and plot stuff

--- cmd/benchmark.sh
+++ cmd/benchmark.sh
@@ -10,11 +10,12 @@ script_set="employees"
 
 # general
 db_host="127.0.0.1"
-MULTIPLICITIES=("10" "20")
+MULTIPLICITIES=("10" "100" "1000" "10000")
 threads=15
 gobench_main_path="/Users/rbo/Documents/Gits/gobench/cmd/main.go"
 script_base_path="/Users/rbo/Documents/Gits/gobench/scripts"
 result_base_path="/Users/rbo/Documents/Gits/gobench/tmp/results"
+chart_type="line"
 
 # mysql
 mysql_port="3306"
@@ -49,8 +50,9 @@ for MULT in "${MULTIPLICITIES[@]}"; do
         --user $mysql_user \
         --pass $mysql_pass \
         --iter $MULT \
+        --threads $threads \
         --script "${script_base_path}/${script_set}/mysql.sql" \
-        --writecsv "${result_base_path}/${script_set}/mysql_${iter}.csv"
+        --writecsv "${result_base_path}/${script_set}/mysql_${MULT}.csv"
 
     echo -e "\nTEST NEO4J"
     go run $gobench_main_path neo4j \
@@ -59,8 +61,9 @@ for MULT in "${MULTIPLICITIES[@]}"; do
         --user $neo_user \
         --pass $neo_pass \
         --iter $MULT \
+        --threads $threads \
         --script "${script_base_path}/${script_set}/neo4j.cql" \
-        --writecsv "${result_base_path}/${script_set}/neo4j_${iter}.csv"
+        --writecsv "${result_base_path}/${script_set}/neo4j_${MULT}.csv"
 
     echo -e "\nTEST POSTGRES"
     go run $gobench_main_path postgres \
@@ -69,10 +72,23 @@ for MULT in "${MULTIPLICITIES[@]}"; do
         --user $postgres_user \
         --pass $postgres_pass \
         --iter $MULT \
+        --threads $threads \
         --script "${script_base_path}/${script_set}/postgres.sql" \
         --writecsv "${result_base_path}/${script_set}/postgres_${MULT}.csv"
 done
 
 echo -e "\n"
+echo $(for i in $(seq 1 50); do printf "#"; done) 
+
+echo -e "\nMERGE RESULTS"
+go run $gobench_main_path mergecsv \
+    --rootDir "${result_base_path}/${script_set}" \
+    --targetFile "${result_base_path}/${script_set}/merged_results.csv" 
+
+echo -e "\nCREATE CHARTS"
+go run $gobench_main_path createcharts \
+    --dataFile "${result_base_path}/${script_set}/merged_results.csv" \
+    --charttype $chart_type
+
 echo $(for i in $(seq 1 50); do printf "#"; done) 
 echo -e "\nTOTAL RUN TIME: " $(expr `date +%s` - $start_time) s
